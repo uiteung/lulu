d5e3e23115222546bb436b5dbd95e03c252a27f9
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-03 10:58:50 +0700

    feat(token) : atur interaksi di web dengan token

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+// Untuk Autentifikasi Login User Tertentu
+import { token } from "../controller/cookies.js";
+
+var header = new Headers();
+header.append("login", token);
+header.append("Content-Type", "application/json");
+
+const requestOptions = {
+  method: "GET",
+  headers: header
+};
