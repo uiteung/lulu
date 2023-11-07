728cffd356030eb6188163d302467b54b766457a
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-07 10:14:45 +0700

    add(getmhslulusan) : template url get mhs lulusan

--- static/js/template/template.js
+++ static/js/template/template.js
@@ -1,2 +1,3 @@
 // Endpoint GET
 export let UrlGetLulusan = "https://lulusan.ulbi.ac.id/lulusan"; // query parameter : tahunid
+export let UrlGetMhsLulusan = "https://lulusan.ulbi.ac.id/lulusan/:mhsw"; // path parameter : mhswid
