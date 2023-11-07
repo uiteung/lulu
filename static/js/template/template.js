98a00e67a423aa0cc66be0f61ce58f5ac42a8721
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-07 16:55:46 +0700

    add(token & cookies) : add token &cookies

--- static/js/template/template.js
+++ static/js/template/template.js
@@ -1,4 +1,6 @@
 // Endpoint GET
+import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
+export const token = CihuyGetCookie("login");
 export let UrlGetLulusan = "https://lulusan.ulbi.ac.id/lulusan"; // query parameter : tahunid
 export let UrlGetMhsLulusan = "https://lulusan.ulbi.ac.id/lulusan/:mhsw"; // path parameter : mhswid
 export let UrlGetIjazahMhs = "https://lulusan.ulbi.ac.id/lulusan/ijazah/:mhsw"; // path parameter : mhswid
