673bdcf25de90af76dabfb3a88d224e6ae6ca157
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-07 10:15:49 +0700

    add(endpoint) : url get ijazah mhs

--- static/js/template/template.js
+++ static/js/template/template.js
@@ -1,3 +1,4 @@
 // Endpoint GET
 export let UrlGetLulusan = "https://lulusan.ulbi.ac.id/lulusan"; // query parameter : tahunid
 export let UrlGetMhsLulusan = "https://lulusan.ulbi.ac.id/lulusan/:mhsw"; // path parameter : mhswid
+export let UrlGetIjazahMhs = "https://lulusan.ulbi.ac.id/lulusan/ijazah/:mhsw"; // path parameter : mhswid
