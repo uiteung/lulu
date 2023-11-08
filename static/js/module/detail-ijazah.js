108f95fbf985dabd8d4f59564d0b1e17020fe1ef
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-08 11:44:07 +0700

    feat(getdetailijazah) : fix success get data ijazah

--- static/js/module/detail-ijazah.js
+++ static/js/module/detail-ijazah.js
@@ -1,38 +1,35 @@
 import { CihuyDataAPI } from "https://c-craftjs.github.io/lulu/api.js";
-import { UrlGetMhsLulusan } from "../template/template.js";
-import { token } from "../template/template.js";
+import { UrlGetMhsLulusan, token } from "../template/template.js";
 
 // Ambil MhsId dari URL
 const urlParams = new URLSearchParams(window.location.search);
-const MhsId = urlParams.get('MhsId')
+const MhsId = urlParams.get('MhsId');
 
 let mahasiswaData;
 
-fetch(UrlGetMhsLulusan + `${MhsId}`, token)
-    .then((result) => {
-        return result.json();
-    })
-    .then((mahasiswa) => {
-        if (mahasiswa) {
-            mahasiswaData = mahasiswa;
+CihuyDataAPI(UrlGetMhsLulusan + `${MhsId}`, token, (error, result) => {
+    if (!error && result.success) {
+        mahasiswaData = result.data;
 
-            // Tampilkan Data Profil Mahasiswa
-            document.getElementById("npm").value = mahasiswaData.MhsId;
-            document.getElementById("nama_mahasiswa").value = mahasiswaData.nama;
-            document.getElementById("nik").value = mahasiswaData.nik;
-            document.getElementById("no_hp").value = mahasiswaData.handphone;
-            document.getElementById("tempat_lahir").value = mahasiswaData.tempatLahir;
-            document.getElementById("tgl_lahir").value = mahasiswaData.tanggalLahir;
-            document.getElementById("email").value = mahasiswaData.email;
+        // Tampilkan Data Profil Mahasiswa
+        document.getElementById("npm").value = mahasiswaData.MhswId;
+        document.getElementById("nama_mahasiswa").value = mahasiswaData.nama;
+        document.getElementById("nik").value = mahasiswaData.nik;
+        document.getElementById("no_hp").value = mahasiswaData.handphone;
+        document.getElementById("tempat_lahir").value = mahasiswaData.tempatLahir;
+        document.getElementById("tgl_lahir").value = mahasiswaData.tanggalLahir;
+        document.getElementById("email").value = mahasiswaData.email;
 
-            // Tampilkan Data Ijazah Mahasiswa
-            document.getElementById("prodi").value = mahasiswaData.prodi;
-            document.getElementById("gelar").value = mahasiswaData.gelar;
-            document.getElementById("npm").value = mahasiswaData.MhsId;
-            document.getElementById("npm").value = mahasiswaData.MhsId;
-            document.getElementById("npm").value = mahasiswaData.MhsId;
-            document.getElementById("npm").value = mahasiswaData.MhsId;
-            document.getElementById("npm").value = mahasiswaData.MhsId;
-            document.getElementById("npm").value = mahasiswaData.MhsId;
-        }
-    })
+        // Tampilkan Data Ijazah Mahasiswa
+        document.getElementById("prodi").value = mahasiswaData.prodi;
+        document.getElementById("gelar").value = mahasiswaData.gelar;
+        document.getElementById("no_ijazah").value = mahasiswaData.noIjazah;
+        document.getElementById("no_sk").value = mahasiswaData.noSkIjazah;
+        document.getElementById("tgl_lulus").value = mahasiswaData.tanggalLulus;
+        document.getElementById("tgl_sk").value = mahasiswaData.tglSKLulus;
+        document.getElementById("judul_ta").value = mahasiswaData.judul_ta;
+        document.getElementById("tahun_id").value = mahasiswaData.tahunId;
+    } else {
+        console.log('Data Ijazah Mahasiswa tidak ditemukan atau terjadi kesalahan.');
+    }
+});
