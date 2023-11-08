be7001fdb4e679c0a624945f35ed051aa9190954
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-08 11:04:21 +0700

    fix(endpoint) : perbaikan endpoint

--- static/js/template/template.js
+++ static/js/template/template.js
@@ -3,5 +3,5 @@ import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 export const token = CihuyGetCookie("login");
 
 export let UrlGetLulusan = "https://lulusan.ulbi.ac.id/lulusan"; // query parameter : tahunid
-export let UrlGetMhsLulusan = "https://lulusan.ulbi.ac.id/lulusan/:mhsw"; // path parameter : mhswid
-export let UrlGetIjazahMhs = "https://lulusan.ulbi.ac.id/lulusan/ijazah/:mhsw"; // path parameter : mhswid
+export let UrlGetMhsLulusan = "https://lulusan.ulbi.ac.id/lulusan/"; // path parameter : mhswid
+export let UrlGetIjazahMhs = "https://lulusan.ulbi.ac.id/lulusan/ijazah/"; // path parameter : mhswid
