18a81e13066eab256aaccc209ce2a696ba544d8b
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-08 15:07:37 +0700

    feat(cetakijazah) : coba cetak di detail ijazah

--- static/js/module/detail-ijazah.js
+++ static/js/module/detail-ijazah.js
@@ -1,6 +1,7 @@
 import { CihuyDataAPI } from "https://c-craftjs.github.io/lulu/api.js";
-import { UrlGetMhsLulusan, token } from "../template/template.js";
+import { UrlGetMhsLulusan, UrlGetIjazahMhs, token } from "../template/template.js";
 
+// Untuk Get Data Ijazah Mahasiswa By Id
 // Ambil MhsId dari URL
 const urlParams = new URLSearchParams(window.location.search);
 const MhsId = urlParams.get('MhsId');
@@ -32,4 +33,29 @@ CihuyDataAPI(UrlGetMhsLulusan + `${MhsId}`, token, (error, result) => {
     } else {
         console.log('Data Ijazah Mahasiswa tidak ditemukan atau terjadi kesalahan.');
     }
+});
+
+// Untuk Cetak Ijazah Mahasiswa by Id
+// Mendefinisikan button untuk cetak terlebih dahulu
+const cetakIjazahButton = document.getElementById("submitCetakIjazah");
+// Untuk Cetak ijazahnya
+cetakIjazahButton.addEventListener("click", () => {
+  fetch(UrlGetMhsLulusan + `${MhsId}`, {
+    headers: {
+        'LOGIN' : token,
+    }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.code === 200 && data.success) {
+        const docId = data.data;
+        const googleDocsUrl = `https://docs.google.com/document/u/0/d/${docId}`;
+        window.open(googleDocsUrl, '_blank');
+      } else {
+        console.error("Gagal mengambil data ijazah.");
+      }
+    })
+    .catch((error) => {
+      console.error("Terjadi kesalahan saat mengambil data ijazah:", error);
+    });
 });
