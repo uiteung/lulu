a7972aac1b0e2ee8701ec8f8002a1b4223aa5db0
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-09 10:39:00 +0700

    feat(getdatadashboard) : get data untuk dashboard

--- static/js/module/index.js
+++ static/js/module/index.js
@@ -1,38 +1,162 @@
+import { CihuyDataAPI } from "https://c-craftjs.github.io/lulu/api.js";
+import { CihuyDomReady, CihuyQuerySelector } from "https://c-craftjs.github.io/table/table.js";
+import { CihuyId } from "https://c-craftjs.github.io/element/element.js";
+import { UrlGetLulusan } from "../template/template.js";
+import { token } from "../template/template.js";
+
+CihuyDomReady(() => {
+  const tablebody = CihuyId("tablebody");
+  const buttonsebelumnya = CihuyId("prevPageBtn");
+  const buttonselanjutnya = CihuyId("nextPageBtn");
+  const halamansaatini = CihuyId("currentPage");
+  const itemperpage = 10;
+  let halamannow = 1;
+
 // Untuk GET All Data Ijazah berdasarkan TahunId
-function getData() {
-  const tahunid = document.getElementById("tahunid").value;
-  const url = `https://lulusan.ulbi.ac.id/lulusan?tahunid=${tahunid}`;
-
-  $.ajax({
-      url: url,
-      method: 'GET',
-      success: function (response) {
-          const tablebody = document.getElementById("tablebody");
-          // Mengosongkan tabel
-          tablebody.innerHTML = "";
-
-          if (response.success) {
-              // Mengisi tabel dengan data yang diterima dari server
-              response.data.forEach(function (item) {
-                  const row = `
-                  <tr>
-                      <td style="text-align: center; vertical-align: middle">${item.MhswId}</td>
-                      <td style="text-align: center; vertical-align: middle">${item.nama}</td>
-                      <td style="text-align: center; vertical-align: middle">${item.nik}</td>
-                      <td style="text-align: center; vertical-align: middle">${item.prodi}</td>
-                      <td style="text-align: center; vertical-align: middle">
-                          <button class="btn btn-success">Cetak Ijazah</button>
-                          <button class="btn btn-primary">Detail</button>
-                      </td>
-                  </tr>`;
-                  tablebody.innerHTML += row;
-              });
+document.addEventListener("DOMContentLoaded", function () {
+    const cariMahasiswaBtn = document.getElementById("cariMahasiswaBtn");
+    const tahunidInput = document.getElementById("tahunid");
+    const tableBody = document.getElementById("tablebody");
+  
+    cariMahasiswaBtn.addEventListener("click", function () {
+      const tahunid = tahunidInput.value;
+      if (tahunid) {
+        const apiUrl = UrlGetLulusan + `?tahunid=${tahunid}`;
+  
+        CihuyDataAPI(apiUrl , token, function (error, data) {
+          if (error) {
+            tableBody.innerHTML = `<tr><td colspan="5">Terjadi kesalahan: ${error.message}</td></tr>`;
           } else {
-              alert('Gagal mengambil data. Status: ' + response.status);
+            // Hapus semua data dari tabel
+            tableBody.innerHTML = "";
+  
+            if (data.success) {
+              // Tambahkan data mahasiswa ke dalam tabel
+              data.data.forEach((mahasiswa) => {
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                  <td style="text-align: center; vertical-align: middle">${mahasiswa.MhswId}</td>
+                  <td style="text-align: center; vertical-align: middle">${mahasiswa.nama}</td>
+                  <td style="text-align: center; vertical-align: middle">${mahasiswa.nik}</td>
+                  <td style="text-align: center; vertical-align: middle">${mahasiswa.prodi}</td>
+                  <td style="text-align: center; vertical-align: middle">
+                    <button type="button" class="btn btn-primary" data-ijazah="${mahasiswa.MhswId}">Cetak Transkrip Nilai</button>
+                    <button type="button" class="btn btn-success" data-ijazah="${mahasiswa.MhswId}">Cetak Ijazah</button>
+                  </td>
+                `;
+                tableBody.appendChild(row);
+              });
+
+            // Untuk Memunculkan Pagination Halamannya
+            displayData(halamannow);
+			      updatePagination();
+              
+            // Menambahkan event listener untuk button "Detail"
+            const detailButtons = document.querySelectorAll('.btn-primary');
+            detailButtons.forEach(button => {
+                button.addEventListener('click', (event) => {
+                    const MhsId = event.target.getAttribute('data-ijazah');
+                    // Mengarahkan ke halaman detail-ijazah.html dengan mengirimkan parameter MhsId
+                    window.open(`detail-ijazah.html?MhsId=${MhsId}`, '_blank');
+                });
+            });
+            
+            // Menambahkan event listener untuk button "Cetak Ijazah"
+            const cetakIjazahButtons = document.querySelectorAll('.btn-success');
+            cetakIjazahButtons.forEach(button => {
+                button.addEventListener('click', (event) => {
+                    const MhsId = event.target.getAttribute('data-ijazah');
+                    const cetakIjazahUrl = `https://lulusan.ulbi.ac.id/lulusan/ijazah/${MhsId}`;
+                    
+                    // Menampilkan Alert Konfirmasi
+                    Swal.fire({
+                        title: 'Konfirmasi Cetak Ijazah',
+                        text: 'Apakah Anda yakin ingin mencetak ijazah?',
+                        icon: 'question',
+                        showCancelButton: true,
+                        confirmButtonText: 'OK',
+                        cancelButtonText: 'Batal',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            // The user confirmed, proceed with fetching and printing the document
+                            fetch(cetakIjazahUrl, {
+                                headers: {
+                                    'LOGIN': token, // Gantilah 'LOGIN' dengan nama header yang sesuai
+                                }
+                            })
+                                .then(response => {
+                                    if (!response.ok) {
+                                        throw new Error('Gagal mengambil data');
+                                    }
+                                    return response.json(); // Mengambil respons dalam format JSON
+                                })
+                                .then(data => {
+                                    // Pastikan respons memiliki atribut "data"
+                                    if (data && data.data) {
+                                        const googleDocsUrl = `https://docs.google.com/document/u/0/d/${data.data}`;
+                                        // Membuka halaman Google Docs di jendela baru
+                                        window.open(googleDocsUrl, '_blank');
+                                        // Menampilkan Alert Success
+                                        Swal.fire({
+                                            title: 'Berhasil',
+                                            text: 'Ijazah berhasil dicetak!',
+                                            icon: 'success',
+                                        });
+                                    } else {
+                                        console.error('Data tidak ditemukan dalam respons.');
+                                        // Tampilkan pesan kesalahan jika data tidak ditemukan dalam respons
+                                    }
+                                })
+                                .catch(error => {
+                                    console.error('Terjadi kesalahan:', error);
+                                    // Tampilkan pesan kesalahan kepada pengguna jika diperlukan
+                                });
+                        }
+                    });
+                });
+            });
+            } else {
+              // Tampilkan pesan kesalahan jika permintaan tidak berhasil
+              tableBody.innerHTML = `<tr><td colspan="5">${data.status}</td></tr>`;
+            }
           }
-      },
-      error: function () {
-          alert('Gagal mengambil data.');
+        });
+        // Fungsi Untuk Menampilkan Data
+        function displayData(page) {
+          const baris = CihuyQuerySelector("#tablebody tr");
+          const mulaiindex = (page - 1) * itemperpage;
+          const akhirindex = mulaiindex + itemperpage;
+          for (let i = 0; i < baris.length; i++) {
+              if (i >= mulaiindex && i < akhirindex) {
+                  baris[i].style.display = "table-row";
+              } else {
+                  baris[i].style.display = "none";
+              }
+          }
+        }
+
+        // Fungsi Untuk Update Pagination
+        function updatePagination() {
+          halamansaatini.textContent = `Halaman ${halamannow}`;
+        }
+        buttonsebelumnya.addEventListener("click", () => { // Button Pagination (Sebelumnya)
+          if (halamannow > 1) {
+              halamannow--;
+              displayData(halamannow);
+              updatePagination();
+          }
+        });
+        buttonselanjutnya.addEventListener("click", () => {// Button Pagination (Selanjutnya)
+          const totalPages = Math.ceil(
+              tablebody.querySelectorAll("#tablebody tr").length / itemperpage
+          );
+          if (halamannow < totalPages) {
+              halamannow++;
+              displayData(halamannow);
+              updatePagination();
+          }
+        });
       }
+    });
   });
-}
+});
