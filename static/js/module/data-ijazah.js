f5913c8f03e109d51e09110e2aa39369f9d9ded0
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-10 14:11:48 +0700

    feat(search) : coba benerin fungsi search

--- static/js/module/data-ijazah.js
+++ static/js/module/data-ijazah.js
@@ -12,6 +12,30 @@ CihuyDomReady(() => {
   const itemperpage = 10;
   let halamannow = 1;
 
+  // Membuat fitur search
+  document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchInput");
+    const tableBody = document.getElementById("tablebody").getElementsByTagName("tr");
+
+    searchInput.addEventListener("input", function () {
+      const searchText = searchInput.value.toLowerCase();
+
+      for (const row of tableBody) {
+        const cells = row.getElementsByTagName("td");
+        let rowMatchesSearch = false;
+
+        for (const cell of cells) {
+          if (cell.textContent.toLowerCase().includes(searchText)) {
+            rowMatchesSearch = true;
+            break;
+          }
+        }
+
+        row.style.display = rowMatchesSearch ? "" : "none";
+      }
+    });
+  });
+
 // Untuk GET All Data Ijazah berdasarkan TahunId
 document.addEventListener("DOMContentLoaded", function () {
     const cariMahasiswaBtn = document.getElementById("cariMahasiswaBtn");
