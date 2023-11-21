c1fe6a27cd33878cba204e76f34f21ab939de189
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-21 15:28:38 +0700

    add(endpoint) : endpoint baru detail transkrip

--- static/js/template/template.js
+++ static/js/template/template.js
@@ -5,3 +5,4 @@ export const token = CihuyGetCookie("login");
 export let UrlGetLulusan = "https://lulusan.ulbi.ac.id/lulusan"; // query parameter : tahunid
 export let UrlGetMhsLulusan = "https://lulusan.ulbi.ac.id/lulusan/"; // path parameter : mhswid
 export let UrlGetIjazahMhs = "https://lulusan.ulbi.ac.id/lulusan/ijazah/"; // path parameter : mhswid
+export let UrlGetMhsTranskrip = "https://lulusan.ulbi.ac.id/lulusan/transkrip/"; // path parameter : mhswid
